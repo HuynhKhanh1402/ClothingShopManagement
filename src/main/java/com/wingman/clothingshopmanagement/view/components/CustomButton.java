58e7e632ffd3ef4900ef9ede7aa3bcf38616b5f7
KhanhHuynh <khanh@duck.com>
2024-05-14 20:56:54 +0700

    first commit

---
+++ src/main/java/com/wingman/clothingshopmanagement/view/components/CustomButton.java
@@ -0,0 +1,140 @@
+package com.wingman.clothingshopmanagement.view.components;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+
+/**
+ *
+ * @author ngoan
+ */
+public class CustomButton extends JButton{
+    
+    private boolean over;
+    private Color color;
+    private Color colorOver;
+    private Color colorClick;
+    private Color borderColor;  
+    private int radius = 0;
+    private int borderSize = 1;
+    
+    public CustomButton() {
+        setColor(Color.WHITE);
+        colorOver = Color.LIGHT_GRAY;
+        colorClick = Color.LIGHT_GRAY;
+        borderColor = Color.BLACK;
+        
+        setContentAreaFilled(false);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent me) {
+                setBackground(colorOver);
+                over = true;
+            }
+
+            @Override
+            public void mouseExited(MouseEvent me) {
+                setBackground(color);
+                over = false;
+            }
+
+            @Override
+            public void mousePressed(MouseEvent me) {
+                setBackground(colorClick);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent me) {
+                if (over) {
+                    setBackground(colorOver);
+                } else {
+                    setBackground(color);
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D)g;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(borderColor);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        g2.setColor(getBackground());
+        g2.fillRoundRect(borderSize, borderSize, getWidth()-borderSize*2, getHeight()-borderSize*2, radius, radius);
+        super.paintComponent(g); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+
+    
+    
+    
+
+    public boolean isOver() {
+        return over;
+    }
+
+    public void setOver(boolean over) {
+        this.over = over;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Color getColorOver() {
+        return colorOver;
+    }
+
+    public void setColorOver(Color colorOver) {
+        this.colorOver = colorOver;
+    }
+
+    public Color getColorClick() {
+        return colorClick;
+    }
+
+    public void setColorClick(Color colorClick) {
+        this.colorClick = colorClick;
+    }
+
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    public void setBorderColor(Color borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    public int getBorderSize() {
+        return borderSize;
+    }
+
+    public void setBorderSize(int borderSize) {
+        this.borderSize = borderSize;
+    }
+
+    
+    
+    
+}
---
+++ src/main/java/com/wingman/clothingshopmanagement/view/components/render/PanelRenderer.java
@@ -0,0 +1,45 @@
+package com.wingman.clothingshopmanagement.view.components.render;
+
+import java.awt.Component;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class PanelRenderer implements TableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+        if (value instanceof JPanel panel) {
+            applyJTableAttributes(table, panel, isSelected);
+        }
+        
+        return (Component) value;
+    }
+    
+    private void applyJTableAttributes(JTable table, JPanel container, boolean isSelected) {
+        if (isSelected) {
+            container.setBackground(table.getSelectionBackground());
+        } else {
+            container.setBackground(table.getBackground());
+        }
+        
+        for (Component component: container.getComponents()) {
+            if (component == null) {
+                continue;
+            }
+            if (component instanceof JPanel panel) {
+                applyJTableAttributes(table, panel, isSelected);
+            } else {
+                if (isSelected) {
+                    component.setBackground(table.getSelectionBackground());
+                    component.setForeground(table.getSelectionForeground());
+                } else {
+                    component.setBackground(table.getBackground());
+                    component.setForeground(table.getForeground());
+                }
+            }
+        }
+    }
+        
+}
